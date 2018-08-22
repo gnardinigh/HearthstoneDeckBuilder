bb6653013f7ae93f830e9e7b89735f5cad307c4c
TylerNodell <Tyler.l.nodell@gmail.com>
2018-08-22 10:00:39 -0400

    Memes

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,9 @@
 class CardsController < ApplicationController
 
   def index
-    @cards = Card.player_class("Mage")
-    @neutral_cards = Card.player_class("Neutral")
-    @selection_range = [{num:0},{num:1},{num:2}]
+    # @cards = Card.player_class("Mage")
+    # @neutral_cards = Card.player_class("Neutral")
+    # @selection_range = [{num:0},{num:1},{num:2}]
   end
 
   def create_deck
