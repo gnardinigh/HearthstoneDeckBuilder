eaeafdf15c7522d8b41037fe27ea07ecb5cef3b7
TylerNodell <Tyler.l.nodell@gmail.com>
2018-08-22 11:23:27 -0400

    Added Bcrpyt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_134514) do
+ActiveRecord::Schema.define(version: 2018_08_22_152214) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_134514) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
