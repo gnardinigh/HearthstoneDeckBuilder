a58de7cc84382a3ad55bd045d22dcc1c4183ac8b
gardiner nardini <gardiner.nardini@gmail.com>
2018-08-22 18:31:18 -0400

    send card choices from decks#new to decks#add_to_deck

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -2,10 +2,10 @@ class DecksController < ApplicationController
 
   def index
     @cards = Card.all
-
   end
 
   def new
+    @deck = Deck.create()
     @chosen_class = params[:commit]
     @cards = Card.player_class(@chosen_class)
     @neutral_cards = Card.player_class("Neutral")
@@ -13,11 +13,14 @@ class DecksController < ApplicationController
   end
 
   def add_to_deck
-
+    params[:num_cards][0].to_i.times do
+      DeckCard.create(deck_id: Deck.last.id, card_id: params[:num_cards][-5..-1].to_i)
+    end
+    byebug
   end
 
   def create
-    byebug
+
   end
 
 
