3791a6d0acecc26b17d1330f81d9873943924678
github username <github email address>
2018-08-21 21:07:52 -0400

    Made Card index page into two columns, player_class left and Neutral right

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,7 @@ class CardsController < ApplicationController
 
   def index
     @cards = Card.player_class("Mage")
-    @heroes = Card.heroes
+    @neutral_cards = Card.player_class("Neutral")
   end
 
 
