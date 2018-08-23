a1e53ee220a4ed313b4c9f8f332c5678c137e52e
gardiner nardini <gardiner.nardini@gmail.com>
2018-08-23 19:22:54 -0400

    all params sent from deck's new.html.erb are now permitted by strong params. Fully functional.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -14,12 +14,12 @@ class DecksController < ApplicationController
 
   def add_to_deck
 
-    @chosen_class = params[:commit]
-    @cid = params[:id]
-    # byebug
+    @chosen_class = card_params[:class]
+    @cid = card_params[:id]
     DeckCard.create(deck_id: Deck.last.id, card_id: @cid)
     flash[:notice] = "Created an instance of #{DeckCard.last.card.name} in deck number #{Deck.last.id}."
     redirect_to new_deck_path(commit: @chosen_class)
+
   end
 
   def create
@@ -29,7 +29,7 @@ class DecksController < ApplicationController
   private
 
   def card_params
-    params.require("card_choice").permit(Card.all.map{|card|card.id.to_s})
+    params.require("commit").permit("class","id")
   end
 
 
