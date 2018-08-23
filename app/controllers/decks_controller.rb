d0b9f74604f9cda871cc7909bb568c1c3a2d6a40
TylerNodell <Tyler.l.nodell@gmail.com>
2018-08-23 15:37:05 -0400

    globaled

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -6,20 +6,22 @@ class DecksController < ApplicationController
   end
 
   def new
-    @@chosen_class = params[:commit]
-    @cards = Card.player_class(@@chosen_class)
+    @chosen_class = params[:commit]
+    @@holdclass = @chosen_class
+    @cards = Card.player_class(@chosen_class)
     @neutral_cards = Card.player_class("Neutral")
     @selection_range = [{num:0},{num:1},{num:2}]
   end
 
   def add_to_deck
-    byebug
+    @chosen_class = @@holdclass
     repeat = card_params.values[0].to_i
+    # byebug
     repeat.times do
       DeckCard.create(deck_id: Deck.last.id, card_id: card_params.keys[0].to_i)
     end
     flash[:notice] = "Created #{repeat} instances of #{DeckCard.last.card.name} in deck number #{Deck.last.id}."
-    redirect_to new_deck_path(commit: @@chosen_class)
+    redirect_to new_deck_path(commit: @chosen_class)
   end
 
   def create
